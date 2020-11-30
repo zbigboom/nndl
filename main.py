8f768876062a74e173df26190c5ab37dc28faf5f
zbigboom <965001339@qq.com>
2020-11-30 15:41:28 +0800

    updata

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 tokern=aac79ed6e0e38be1c7c393128bec75104954abf1
+
+c:\\Windows\system32\drivers\etc\hosts
+# GitHub Start
+140.82.114.3    github.com
+140.82.112.3    gist.github.com
+140.82.112.6    api.github.com
+185.199.108.153    assets-cdn.github.com
+199.232.68.133    raw.githubusercontent.com
+199.232.68.133    gist.githubusercontent.com
+199.232.68.133    cloud.githubusercontent.com
+199.232.68.133    camo.githubusercontent.com
+199.232.68.133    avatars0.githubusercontent.com
+199.232.68.133    avatars1.githubusercontent.com
+199.232.68.133    avatars2.githubusercontent.com
+199.232.68.133    avatars3.githubusercontent.com
+199.232.68.133    avatars4.githubusercontent.com
+199.232.68.133    avatars5.githubusercontent.com
+199.232.68.133    avatars6.githubusercontent.com
+199.232.68.133    avatars7.githubusercontent.com
+199.232.68.133    avatars8.githubusercontent.com
+ # GitHub End
